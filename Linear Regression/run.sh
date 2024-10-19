91cd5e0ec4b652bafc1aff69bd4c6610ab13cb03
raogeorge <raogeo8@gmail.com>
2024-10-18 23:56:16 -0600

    adding sh files

---
+++ Linear Regression/run.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+echo "Q2 (a)"
+python3 adaboosting.py
+
+echo "Q2 (b)"
+python3 Bagging.py
+
+echo "Q2(c)"
+python3 Bagging vs single tree.py
+
+echo "Q2 (d)"
+python3 random forest.py
+
+echo "Q2 (e)"
+python3 Bagging vs single tree.py
+
+echo "Q3"
+python3 credit_default.py
+
+echo "Q4 (a)"
+python3 Q4(a).py
+
+echo "Q4 (b)"
+python3 Q4(b).py
+
+echo "Q4 (c)"
+python3 Q4(c).py
