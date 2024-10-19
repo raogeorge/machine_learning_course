71c72594cab1968cb113973484441cd2ff13a020
raogeorge <raogeo8@gmail.com>
2024-10-18 23:42:27 -0600

    name change

---
+++ Emsemble Learning/run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "Part 2 Q2 on the car dataset"
+python3 Decision_tree_practice_Q2.py
+
+echo "Part 2 Q3(a) on the car dataset"
+python3 Decision_tree_practice_Q3.py
+
+echo "Part 2 Q3(b) on the car dataset"
+python3 Decision_tree_practice_Q3(b).py
